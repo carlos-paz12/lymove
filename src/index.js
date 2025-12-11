12948ace6181fea6acfbfe897b2be38826e24c39
carlos-paz12 <carlos.silva.1204@proton.me>
2025-12-11 15:39:53 -0300

    Restructure project filesystem for improved modularity

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const gallery = document.getElementById("gallery");
 
 async function loadImages() {
-  const response = await fetch("./assets/images/data.json");
+  const response = await fetch("./src/data/data.json");
   const images = await response.json();
 
   images.sort(() => Math.random() - 0.5);
@@ -12,17 +12,16 @@ async function loadImages() {
 
     wrapper.innerHTML = `
       <div class="relative overflow-hidden rounded-xl group">
-        <img src="./assets/images/${img.src}" class="w-full transition-transform duration-300 group-hover:scale-110" />
+        <img src="./src/assets/images/${img.src}" class="w-full transition-transform duration-300 group-hover:scale-110" />
         <div class="absolute inset-0 bg-black/40 opacity-0 transition-opacity duration-300 group-hover:opacity-100"></div>
       </div>
       <div class="flex justify-between items-center p-2 text-sm">
         <span>${img.name}</span>
-        <a href="./assets/images/${img.name}" download class="cursor-pointer">
+        <a href="./src/assets/images/${img.name}" download class="cursor-pointer">
           <i class="fa-regular fa-floppy-disk"></i>
         </a>
       </div>
-    `;
-
+    `
     gallery.appendChild(wrapper);
   });
 }
